a981c9a5c7c3b2014ef43910899c65ba2fd3e5aa
Casa-Chirilagua <database@casachirilagua.org>
2024-06-05 16:01:25 -0400

    Added new border to side navigation bar

--- app/dashboard/student/loading.jsx
+++ app/dashboard/student/loading.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 const loading = () => {
   return (
-    <div>Loading...</div>
-  )
+    <div className='dash-container'>
+      <div className=" flex flex-col col-span-4 bg-gray-300 h-10 rounded animate-pulse">
+      </div>
+      <NumberCardSkeleton />
+      <NumberCardSkeleton />
+      <NumberCardSkeleton />
+      <BarChartCardSkeleton />
+    </div>);
 }
 
 export default loading
